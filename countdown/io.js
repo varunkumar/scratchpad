3516c2c380c4dc1b7a5cbc4d8dc041740a010c0e
Varunkumar <varunkumar.n@gmail.com>
2011-01-30 23:42:38 +0530

    Updated the correct date

--- countdown/io.js
+++ countdown/io.js
@@ -770,7 +770,7 @@ var digitColors = ['265897', '265897', '265897', '',
   /**
     * Get the date we're counting down to.
     */
-  var countdownTo = new Date(2011, 1, 18, 7, 30, 0).getTime();
+  var countdownTo = new Date(2011, 1, 19, 8, 30, 0).getTime();
 
   /**
     * Buckets for the Digits.
